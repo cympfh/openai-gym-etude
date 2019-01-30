7699c0a48d104b3bb14391c4d8fba99ea4bf9ca1
cympfh <cympfh@gmail.com>
2019-01-30 21:36:49 +0900

    add test subcommand

---
+++ cartpole-v1/README.md
@@ -0,0 +1,12 @@
+# CartPole-v1
+
+```bash
+pip install gym click
+
+# simple manual rule
+python ./manual.py debug
+(shows verbose logs and rendering)
+
+python ./manual.py test  # shows the results of 100 episodes
+average=500.0 max=500.0 min=500.0
+```
--- cartpole-v1/manual.py
+++ cartpole-v1/manual.py
@@ -1,28 +1,66 @@
+import click
 import gym
 
 
-env = gym.make('CartPole-v1')
-env = gym.wrappers.Monitor(env, 'videos/', force=True)
+def episode(env, render=False, verbose=False, max_iterate=500):
 
-obs = env.reset()
-print(obs)
-env.render()
+    obs = env.reset()
+    reward_sum = 0
 
-reward_sum = 0
+    for t in range(max_iterate):
 
-for t in range(1000):
+        # make action
+        x, x_dot, theta, theta_dot = obs
+        action = 0 if theta + 0.1 * theta_dot < 0 else 1
 
-    # make action
-    x, x_dot, theta, theta_dot = obs
-    action = 0 if theta + theta_dot < 0 else 1
+        # take action
+        obs, reward, done, info = env.step(action)
+        reward_sum += reward
 
-    # take action
-    obs, reward, done, info = env.step(action)
-    reward_sum += reward
-    print(t, action, obs, reward, info)
-    env.render()
+        if verbose:
+            click.secho(
+                    f'time={t} '
+                    f'action={action} '
+                    f'observation={obs} '
+                    f'reward={reward} ', err=True)
 
-    if done:
-        print(f'DONE, reward={reward_sum}')
-        env.close()
-        exit()
+        if render:
+            env.render()
+
+        if done:
+            break
+
+    env.close()
+    return reward_sum
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+def debug():
+    """Run 1 episode with rendering and saving video"""
+    env = gym.make('CartPole-v1')
+    env = gym.wrappers.Monitor(env, 'videos/', force=True)
+    reward = episode(env, render=True, verbose=True)
+    print(f'Reward: {reward}')
+
+
+@main.command()
+def test():
+    """Run 100 episodes, then report results"""
+    env = gym.make('CartPole-v1')
+
+    results = []
+    for _ in range(100):
+        results.append(episode(env, render=False, verbose=False))
+
+    print(f'average={sum(results) / len(results)} '
+          f'max={max(results)} '
+          f'min={min(results)}')
+
+
+if __name__ == '__main__':
+    main()
